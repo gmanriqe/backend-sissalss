9f448a617ee5138d3f981a8bc494abb427640927
Jesús Gonzales <jgonzales_albo@albo.com.pe>
2022-05-22 04:31:26 -0500

    API MEDIOS DE PAGO - listar

---
+++ src/routes/medios-pago.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const mysqlConnection = require('../database.js');
+
+// GET all Medios Pago
+const listMediosPago = (req, res) => {
+    const query = `SELECT * FROM medios_pago order by nombre ASC`;
+
+    mysqlConnection.query(query, (err, rows, fields) => {
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+}
+
+router.get('/api/v1/list_medios_pago', listMediosPago)
+
+module.exports = router;
