4fae12036857eee1968e27a97e7350ec5694d95a
gmanriqe <gmanriqe@gmail.com>
2022-06-13 19:01:25 -0500

    BCRYPT - comparando con contrasenia hasheada

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const jwt = require('jsonwebtoken'); // JWT
+const jwt = require('jsonwebtoken'); // Token generator
+const bcrypt = require('bcrypt'); // Password hashing
 
 const mysqlConnection = require('../database.js');
 
 router.post('/api/v1/login', (req, res) => {
     const { username, password } = req.body;
 
-    const query = `SELECT * FROM usuario WHERE usuario = '${username}' AND contrasenia = '${password}'`;
+    // 1ero: Consulto a la BD si existe el usuario
+    const query = `SELECT * FROM usuario WHERE usuario = '${username}'`;
 
     mysqlConnection.query(query, (err, rows, fields) => {
-        console.log(rows)
         if (err) {
             res.status(500).json({
                 status: '500',
@@ -20,18 +21,38 @@ router.post('/api/v1/login', (req, res) => {
             });
         }
 
+        // 2do: Bcryp para comparar la contraseña
         if (rows.length > 0) {
-            const token = jwt.sign({ id: rows[0].id }, 'secret', { expiresIn: 5 * 60 });
+            bcrypt.compare(password, rows[0].contrasenia, (err, result) => {
+                if (err) {
+                    res.status(500).json({
+                        status: '500',
+                        message: 'Error al iniciar sesion',
+                        error: err,
+                    });
+                }
 
-            res.status(200).json({
-                status: '200',
-                message: 'Inicio de sesion exitoso',
-                token: token,
+                if(result) {
+                    // 3ro: Genero el token
+                    const token = jwt.sign({ id: rows[0].id }, 'secret', { expiresIn: 5 * 60 });
+        
+                    res.status(200).json({
+                        status: '200',
+                        message: 'Inicio de sesion exitoso',
+                        token: token,
+                    });
+                }
+                else {
+                    res.status(401).json({
+                        status: '401',
+                        message: 'Contraseña incorrecta',
+                    });
+                }
             });
         } else {
             res.status(401).json({
                 status: '401',
-                message: 'Usuario o contraseña incorrectos',
+                message: 'Usuario no existe',
             });
         }
     })
